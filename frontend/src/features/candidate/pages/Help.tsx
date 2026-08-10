c1ee1dc739b3393c1300339f9dc98c60aaa5467c
Rks052004 <rksrivastava23-cse@bvucoep.edu.in>
2026-08-10 11:23:45 +0530

    Help FAQ: change withdraw-application answer from Yes to No

--- frontend/src/features/candidate/pages/Help.tsx
+++ frontend/src/features/candidate/pages/Help.tsx
@@ -31,7 +31,7 @@ export function Help() {
     },
     {
       question: "Can I withdraw an application after submitting it?",
-      answer: "Yes. Open the application from 'My Applications' and select 'Withdraw'. This is permanent and the recruiter will be notified.",
+      answer: "No. Open the application from 'My Applications' and select 'Withdraw'. This is permanent and the recruiter will be notified.",
     },
   ]
 
